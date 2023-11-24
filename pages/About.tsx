e6c1dc007b821dc46fec6109bc1332802b6730b1
beckjosh <Joshbeck17@gmail.com>
2023-11-24 11:04:21 +0100

    "Adjusted Next/Image"

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,5 +1,5 @@
-import { StaticImageData } from "next/image";
-import Image from "next/image";
+import { StaticImageData } from "next/legacy/image";
+import Image from "next/legacy/image";
 import React from "react";
 
 interface AboutStructure {
